40107876178ab9f0d8383c6b2a438c8f4e2858df
herdi-nooor <herdinoor369@gmail.com>
2022-10-11 20:58:03 +0800

    fdsfsf

--- Assets/Scripts/Scene/Gameplay/Weapon/Projectile/ProjectileInject.cs
+++ Assets/Scripts/Scene/Gameplay/Weapon/Projectile/ProjectileInject.cs
@@ -3,12 +3,12 @@ using UnityEngine;
 namespace Madicine.Scene.Gampalay.Weapons{
     public class ProjectileInject : BaseProjectile {
         [SerializeField] private float _speed;
-        private float timing = 3f;
+        private float timing = 2f;
 
         private void Update() {
             if(timing < 0){
                 DestroyProjectile();
-                timing = 3f;
+                timing = 2f;
             }else{timing -= Time.deltaTime;}
         }
 
